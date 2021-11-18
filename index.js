c3e5e1077553a8c227a9c0b461b9bd849af2db36
maoforero <maforeror@gmail.com>
2021-11-18 02:30:15 -0500

    rutas creadas

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ let expres = require("express");
 let app = expres();
 const PORT = 3000;
 
-app.get("/", (req, res, next) => {
-    res.send("Hola Mundo");
-}).listen(PORT, () =>{
+app.get("/productos", (req, res, next) => {
+    res.send("Productos");
+});
+
+app.get("/productoRandom", (req, res, next) => {
+    res.send("Productos Ramdon")
+})
+
+app.listen(PORT, () =>{
     console.log(`server: http://localhost:${PORT}`)
 })
